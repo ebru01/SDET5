7fe010bce47043880345ead49e26ead51316f4c2
TechnoStudy <ismettechno@gmail.com>
2023-02-20 16:51:41 +0000

    _02_FindingById

---
+++ src/Gun04/_02_FindingById.java
@@ -0,0 +1,22 @@
+package Gun04;
+
+import Utlity.MyFunc;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _02_FindingById {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://form.jotform.com/221934510376353"); // sayfaya gittim
+
+        WebElement name = driver.findElement(By.id("first_8")); // elemanı id İLE bul
+
+
+
+        MyFunc.Bekle(5);
+        driver.quit();
+    }
+}
