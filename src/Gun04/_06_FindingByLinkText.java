f89eca8843489ff0855e3bde171f836223a2e867
TechnoStudy <ismettechno@gmail.com>
2023-02-21 17:23:49 +0000

    BaseDriver

--- src/Gun04/_06_FindingByLinkText.java
+++ src/Gun04/_06_FindingByLinkText.java
@@ -11,9 +11,18 @@ public class _06_FindingByLinkText {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.hepsiburada.com/"); // sayfaya gittim
 
-        WebElement siparislerimLinki= driver.findElement(By.linkText("Siparişlerim"));
+        // LinkText yoluyla eleman bulma işlemi sadece html <a> tag inde kullanılabilir.
+        WebElement siparislerimLinki= driver.findElement(By.linkText("Siparişlerim")); // tam görünen link text ini vereceğiz
         // gözüken text i Siparişlerim olan a tag li webelement
         System.out.println("siparislerimLinki.getText() = " + siparislerimLinki.getText());
+
+        System.out.println("siparislerimLinki.getAttribute(href) = " + siparislerimLinki.getAttribute("href"));
+        System.out.println("siparislerimLinki.getAttribute(title) = " + siparislerimLinki.getAttribute("title"));
+        System.out.println("siparislerimLinki.getAttribute(rel) = " + siparislerimLinki.getAttribute("rel"));
+
+        // partialLinkText
+        WebElement link2= driver.findElement(By.partialLinkText("Süper Fiyat")); // görünen link text inin bir parçası verilebiliyor
+        System.out.println("link2.getText() = " + link2.getText());
 
         MyFunc.Bekle(3);
         driver.quit();
